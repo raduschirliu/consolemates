41e15cee4fdebf3f8f1e8857927b7fa12a274d46
Tyler Yip <tyler.yip1@ucalgary.ca>
2021-11-13 22:42:54 -0700

    feat: added list letters and list topics

--- frontend/src/pages/TerminalPage.tsx
+++ frontend/src/pages/TerminalPage.tsx
@@ -1,14 +1,20 @@
 import { Modal } from '@mui/material';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import ILetter from '../models/Letter';
+import LetterContext from '../contexts/LetterContext';
 import LetterEditor from 'src/components/LetterEditor/LetterEditor';
 import Terminal from 'terminal-in-react';
+import ITopic from 'src/models/Topic';
 
 const TerminalPage = () => {
   const [editorOpen, setEditorOpen] = useState<boolean>(false);
+  const [freshLetters, setFreshLetters] = useState<ILetter[]>([]);
+  const [topics, setTopics] = useState<ITopic[]>([]);
+  const { getNewLetters, markLetterViewed, getAllTopics } =
+    useContext(LetterContext);
 
   return (
-    <div className="flex-grow">
-      <h1 className="text-center text-blue-500">Hello</h1>
+    <div className="h-full w-full">
       <Terminal
         color="green"
         backgroundColor="black"
@@ -16,6 +22,32 @@ const TerminalPage = () => {
         hideTopBar={true}
         allowTabs={false}
         commands={{
+          ls: (args: string[], print: any, runCommand: any) => {
+            const arg = args[1];
+            if (arg === '-l') {
+              // if the letter flag is set
+              getNewLetters()
+                .then((result) => {
+                  setFreshLetters(result);
+                  result.map((letter, index) =>
+                    print(create_letter_string(letter, index))
+                  );
+                })
+                .catch(alert);
+            } else if (arg === '-t') {
+              // if the topic flag is set
+              getAllTopics()
+                .then((result) => {
+                  setTopics(result);
+                  result.map((topic, index) =>
+                    print(create_topic_string(topic, index))
+                  );
+                })
+                .catch(alert);
+            } else {
+              print('');
+            }
+          },
           stonks: () => 'stonks',
           touch: () => setEditorOpen(true),
         }}
@@ -26,5 +58,17 @@ const TerminalPage = () => {
     </div>
   );
 };
+
+function create_letter_string(letter: ILetter, index: Number) {
+  if (letter.reply_id == null) {
+    return '(' + index.toString() + ') ' + letter.subject;
+  } else {
+    return '(' + index.toString() + ') RE: ' + letter.subject;
+  }
+}
+
+function create_topic_string(topic: ITopic, index: number): any {
+  return '(' + index.toString() + ') ' + topic.name;
+}
 
 export default TerminalPage;
